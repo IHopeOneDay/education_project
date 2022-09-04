c37dacef6bc30279457156f77eefa45bc44f0b1e
IHopeOneDay <emremertisinbusiness@gmail.com>
2022-09-04 17:43:58 +0300

    radio selection in login form is added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const [radio_1, radio_2] = document.querySelectorAll("input[type=radio]");
+const signin_button = document.querySelector(".signin-buttons");
+const login_buttons = document.querySelector(".login-buttons");
+const password_repeat = document.querySelector(".password-repeat-div");
 
-console.log(radio_1, radio_2);
+radio_1.addEventListener("click", () => {
+  login_buttons.classList.remove("is-hiding");
+  signin_button.classList.add("is-hiding");
+  password_repeat.classList.add("is-hiding");
+});
+radio_2.addEventListener("click", () => {
+  login_buttons.classList.add("is-hiding");
+  signin_button.classList.remove("is-hiding");
+  password_repeat.classList.remove("is-hiding");
+});
